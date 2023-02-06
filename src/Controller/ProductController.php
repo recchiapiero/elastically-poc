d72a3ad6def95f0d7ba491f975c951b08a700de0
recchia <piero.recchia@gmail.com>
2023-02-06 10:59:46 -0300

    Added search example

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,20 +2,32 @@
 
 namespace App\Controller;
 
+use App\Form\SearchType;
 use App\Interactors\SearchService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ProductController extends AbstractController
 {
     #[Route('/', name: 'app_product', methods: ['GET', 'POST'])]
-    public function index(SearchService $searchService): Response
+    public function index(Request $request, SearchService $searchService): Response
     {
-        $products = $searchService->search('Games');
+        $form = $this->createForm(SearchType::class);
+        $products = [];
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $data = $form->getData();
+            $products = $searchService->search($data['search']);
+        }
 
         return $this->render('product/index.html.twig', [
             'products' => $products,
+            'form' => $form->createView(),
         ]);
     }
 }
---
+++ src/OpenSearch/DocumentExchanger.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\OpenSearch;
+
+use App\Dto\Product;
+use App\Repository\ProductRepository;
+use Elastica\Document;
+use JoliCode\Elastically\Messenger\DocumentExchangerInterface;
+
+class DocumentExchanger implements DocumentExchangerInterface
+{
+    private ProductRepository $productRepository;
+
+    public function __construct(ProductRepository $productRepository)
+    {
+        $this->productRepository = $productRepository;
+    }
+
+    public function fetchDocument(string $className, string $id): ?Document
+    {
+        if ($className === Product::class) {
+            $product = $this->productRepository->find($id);
+
+            if ($product) {
+                return new Document($id, $product->toDto());
+            }
+        }
+
+        return null;
+    }
+}
