0f02688944dbcff2d45e64c67f1431c29f5e7b4d
recchia <piero.recchia@gmail.com>
2023-02-06 20:14:28 -0300

    Fixed issue with mapping

---
+++ migrations/Version20230206231126.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20230206231126 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE offer ALTER price TYPE NUMERIC(6, 2)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('ALTER TABLE offer ALTER price TYPE DOUBLE PRECISION');
+    }
+}
